5967a65d45a6545230fd92d4edd114050a642ec6
Ｍindy <mindy8503020312@gmail.com>
2017-03-01 14:55:26 +0800

    上傳圖片

--- src/main/java/com/Lily/project/controller/HomeController.java
+++ src/main/java/com/Lily/project/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.Lily.project;
+package com.Lily.project.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "upload";
 	}
 	
 }
